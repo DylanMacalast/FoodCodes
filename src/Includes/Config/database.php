d60090d98d5db9c4a06d30319cbc071a4592780a
DylanMacalast <dylanmacalast1@gmail.com>
2019-10-15 22:27:51 +0100

    testing connection to db

---
+++ src/Includes/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "eu-cdbr-west-02.cleardb.net";
+$username = "bddfa11e02d9a4";
+$password = "5ec34cb5";
+$database = "heroku_7d55445124d8085";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
