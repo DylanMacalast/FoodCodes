6f7dc85fbcb76a6d8d52eb44ae7c7a40c52d377a
DylanMacalast <dylanmacalast1@gmail.com>
2019-10-16 21:24:42 +0100

    hot fix

--- src/Includes/Config/database.php
+++ src/Includes/Config/database.php
@@ -12,4 +12,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+$query = "SELECT * FROM users"; 
+        $select_users= mysqli_query($conn, $query);
+       var_dump(mysqli_fetch_assoc($select_users));
+        while($row = mysqli_fetch_assoc($select_users)){
+            $name = row['name'];
+            var_dump($row);
+
+        }
+        echo $name;
 ?>
